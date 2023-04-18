c9e182354234d713c937a0fd4ff68f52329470ff
Yaroslav <aim4rip@gmail.com>
2023-04-18 09:59:25 +0300

    --ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,31 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        Work obj = new Work();
-        boolean res = obj.func(3, 18, 10, 8);
 
-        String msg=res?"At least one sum aliquot to 2":"There are no aliquot values";
-        System.out.println(msg);
+        String str = "Alissandritto";
+        var toOut = uniqueChars(str);
+
+        toOut.values().forEach(System.out::println);
+    }
+
+    public static Map<Integer,Character> uniqueChars(String str){
+        char[] ch_arr=str.toCharArray();
+        Map<Integer,Character> unique = new HashMap<>();
+
+        for(int i=0;i<ch_arr.length;i++) {
+            for(int x=0;x<ch_arr.length;x++)
+                if(i!=x){
+                    if(ch_arr[i]!=ch_arr[x]){
+                        if(!unique.containsValue(ch_arr[i])){
+                            unique.put(i,ch_arr[i]);
+                        }
+                    }
+                }
+        }
+        return unique;
     }
 }
