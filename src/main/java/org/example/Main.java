2be6eac8a0e3956a0a702b5b6a8b5e94885c3464
Yaroslav <aim4rip@gmail.com>
2023-04-18 23:43:04 +0300

    --ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,13 +10,26 @@ public class Main {
         var toOut = uniqueChars(str);
 
         toOut.values().forEach(System.out::println);
+
+        int[]numbers = new int []{8,2,3,4,5,6,7};
+        var sumEven = evenInt(numbers);
+        System.out.println(sumEven);
+        //List<String> ls = new LinkedList<>(){"sih", "rui"};
+
+        //Map<Integer,List<Object>> MapStr = new HashMap<>();
     }
 
     public static Map<Integer,Character> uniqueChars(String str){
         char[] ch_arr=str.toCharArray();
         Map<Integer,Character> unique = new HashMap<>();
 
-        for(int i=0;i<ch_arr.length;i++) {
+        for (int i = 0; i < ch_arr.length; i++) {
+            if (!unique.containsValue(ch_arr[i])) {
+                unique.put(i, ch_arr[i]);
+            }
+        }
+
+     /*   for(int i=0;i<ch_arr.length;i++) {
             for(int x=0;x<ch_arr.length;x++)
                 if(i!=x){
                     if(ch_arr[i]!=ch_arr[x]){
@@ -25,7 +38,17 @@ public class Main {
                         }
                     }
                 }
-        }
+        }*/
         return unique;
+    }
+
+    public static int evenInt(int[]arr){
+        int res=0;
+        for(int c:arr){
+            if(c%2==0){
+                res+=c;
+            }
+        }
+        return res;
     }
 }
